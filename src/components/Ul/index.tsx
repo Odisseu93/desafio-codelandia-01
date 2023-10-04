5a8f46e60f8404256ad13b4166627ffb8008dfa7
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-10-04 19:36:56 -0300

    feat: add Ul component

---
+++ src/components/Ul/index.tsx
@@ -0,0 +1,11 @@
+type UlProps = {
+  childrenList: React.ReactNode[]
+} & React.ComponentProps<'ul'>
+
+const Ul = ({ childrenList, ...rest }: UlProps) => (
+  <ul {...rest}>
+    {childrenList.map((children, index: number) => <li key={index}>{children}</li>)}
+  </ul>
+)
+
+export default Ul
